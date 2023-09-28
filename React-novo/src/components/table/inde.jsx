020de42f3039aeb64a8d819b7ff93da39ec95940
ICARO AUGUSTO VIEIRA BACCHI <ICARO0532@DIGITALCOLLEGE.LOCAL>
2023-09-28 19:51:54 -0300

    Projeto inicial

---
+++ React-novo/src/components/table/inde.jsx
@@ -0,0 +1,33 @@
+export default function Table({ products }) {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Imagem</th>
+                        <th>Titulo</th>
+                        <th>Categoria</th>
+                        <th>Preço</th>
+                        <th>Avaliação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product) => {
+                            return (
+                                <tr key={product.id}>
+                                    <td>{product.id}</td>
+                                    <td>{product.image}</td>
+                                    <td>{product.title}</td>
+                                    <td>{product.category}</td>
+                                    <td>{product.price}</td>
+                                    <td>{product.rating.rate}</td>
+                                </tr>
+                            );
+                        })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
---
+++ React-novo/src/pages/Home/index.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Table from "../../components/table/inde";
+
+
+export default function Home() {
+    const [products, setProducts] = useState([])
+    async function getAllProducts() {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        if (response.data) {
+            setProducts(response.data);
+        }
+    }
+
+    useEffect(() => {
+        getAllProducts();
+    }, []);
+
+    return (
+        <>
+        <h1>Página principal</h1>
+        <div>
+            <Table products={products} />
+        </div>
+    </> 
+    );
+}
